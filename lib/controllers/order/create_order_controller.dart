4bb7baaa8492048287ce435e62e920592610ee4f
EwigLife <zain@ewiglife.com>
2022-06-11 21:08:37 +0500

    orders are creating

--- lib/controllers/order/create_order_controller.dart
+++ lib/controllers/order/create_order_controller.dart
@@ -95,7 +95,7 @@ class CreateOrderController extends GetxController {
           "Basic Y2tfNTQyNDFkODc1NjY2OTY1ODZlY2Q1NGIxZWQ3MmE3YzY5ZWIwN2Y4Njpjc19kN2RkMzFlZGJiMDEyNWZjMWM5ZTU0ZTk2YTY3MzFiNWQ5NmY3Nzg4",
       "consumer_secret": "cs_d7dd31edbb0125fc1c9e54e96a6731b5d96f7788",
       "consumer_key": "ck_54241d87566696586ecd54b1ed72a7c69eb07f86",
-      "host": "clfbooks.childrensliteraturefestival.com"
+      "host": "itapublications.pakistanlearningfestival.com"
       // "nonce": nonceString
     };
     var detail = await http
